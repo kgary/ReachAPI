4af2fcab5d313cc9d27f687901228535dbbc1781
Mohit Mulchandani <Mohit.Mulchandani@asu.edu>
2018-02-19 17:27:40 -0700

    Task #177 - Write skeleton for MongoDB implementation of DAO and include stub code for creating connections by using dao.properties

---
+++ src/edu/asu/heal/core/api/dao/impl/MongoDBDAO.java
@@ -0,0 +1,105 @@
+package edu.asu.heal.core.api.dao.impl;
+
+import com.mongodb.*;
+import edu.asu.heal.core.api.dao.DAO;
+import edu.asu.heal.core.api.dao.DAOException;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class MongoDBDAO implements DAO {
+    private String __mongoUser;
+    private String __mongoPassword;
+    private String __mongoHost;
+    private int __mongoPort;
+    private String __mongoDBName;
+    private String __mongoURI;
+
+    public MongoDBDAO() {}
+
+    public MongoDBDAO(Properties properties){
+        __mongoURI = properties.getProperty("mongo.uri");
+
+        // Alternatively, the following properties can be used if we don't want to deal with URIs separately
+//        __mongoUser = properties.getProperty("mongo.username");
+//        __mongoPassword = properties.getProperty("mongo.password");
+//        __mongoHost = properties.getProperty("mongo.host");
+//        __mongoPort = Integer.parseInt(properties.getProperty("mongo.port"));
+//        __mongoDBName = properties.getProperty("mongo.dbname");
+    }
+
+    private MongoClient getConnection(){
+        try {
+            return new MongoClient(new MongoClientURI(__mongoURI));
+
+            //Alternatively the following lines of code can be used
+//            MongoCredential credential = MongoCredential.createCredential(__mongoUser, __mongoDBName, __mongoPassword.toCharArray());
+//            MongoClient client = new MongoClient(new ServerAddress(__mongoHost, __mongoPort), credential, MongoClientOptions.builder().build());
+        }catch (Exception e){
+            System.out.println("SOME ERROR GETTING MONGO CONNECTION");
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    @Override
+    public Object getScheduledActivities(int currentDay) throws DAOException {
+        return null;
+    }
+
+    @Override
+    public boolean scheduleSTOPActivity(int day, boolean completed) throws DAOException {
+        return false;
+    }
+
+    @Override
+    public boolean scheduleSTICActivity(int day, int sticVariable) throws DAOException {
+        return false;
+    }
+
+    @Override
+    public boolean scheduleRelaxationActivity(int day, boolean completed) throws DAOException {
+        return false;
+    }
+
+    @Override
+    public boolean scheduleDailyDiaryActivity(String dailyDiaryWeeklySchedule) {
+        return false;
+    }
+
+    @Override
+    public boolean scheduleABMTActivity(int day, boolean completed) throws DAOException {
+        return false;
+    }
+
+    @Override
+    public boolean scheduleWorryHeadsActivity(String worryHeadsWeeklySchedule) {
+        return false;
+    }
+
+    @Override
+    public boolean scheduleSAFEACtivity(int day, boolean completed) throws DAOException {
+        return false;
+    }
+
+    @Override
+    public Object getMakeBelieveActivityInstance() throws DAOException {
+        return null;
+    }
+
+    @Override
+    public boolean checkSituationExists(int situationId) throws DAOException {
+        return false;
+    }
+
+    @Override
+    public Object getMakeBelieveActivityAnswers(int situationId) throws DAOException {
+        return null;
+    }
+
+    @Override
+    public boolean updateMakeBelieveActivityInstance(Object makeBelieveResponse) throws DAOException {
+        return false;
+    }
+}
