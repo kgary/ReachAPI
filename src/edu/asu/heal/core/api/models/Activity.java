bd2b1ce1202fe92a772874a4a66416ef71d3d7b1
Dipak Purbey <dpak005@gmail.com>
2018-02-19 19:33:09 -0700

    Task #184 Implement POJO for Activity Model

--- src/edu/asu/heal/core/api/models/Activity.java
+++ src/edu/asu/heal/core/api/models/Activity.java
@@ -1,4 +1,31 @@
 package edu.asu.heal.core.api.models;
 
 public class Activity {
+
+    private String title;
+    private String description;
+
+    public Activity(String title, String description){
+        this.title = title;
+        this.description = description;
+    }
+
+    // getters and setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
