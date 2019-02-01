3e1410d7d4d8811487b1668a4c61f20fc870296d
Abhishek <abhishek.uaf30@gmail.com>
2019-01-31 23:42:39 -0700

    Unique random number generation for notification id

--- src/edu/asu/heal/core/api/models/NotificationData.java
+++ src/edu/asu/heal/core/api/models/NotificationData.java
@@ -1,5 +1,8 @@
 package edu.asu.heal.core.api.models;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Random;
 
 import javax.annotation.Generated;
@@ -10,15 +13,19 @@ public class NotificationData implements IHealModelType {
     private String detail;
     private int id;
     private String title;
-    private static Random __r = new Random();
+    private static ArrayList<Integer> values = new ArrayList<Integer>();
+    private static int MAX_VAL = 10000000; // this number can be configurable .
+    
     public NotificationData() {
         super();
-        id = __r.nextInt(100000);
+        id = values.get(MAX_VAL);
+        MAX_VAL--;
     }
 
     public NotificationData(String detail, String title) {
         super();
-        id = __r.nextInt(100000);
+        id = values.get(MAX_VAL);
+        MAX_VAL--;
         this.detail = detail;
         this.title = title;
     }
@@ -37,5 +44,14 @@ public class NotificationData implements IHealModelType {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+    
+    static {
+    	
+    	for(int i=0; i<MAX_VAL; i++) {
+    		values.add(i);
+    	}
+    	Collections.shuffle(values);
+    	
     }
 }
