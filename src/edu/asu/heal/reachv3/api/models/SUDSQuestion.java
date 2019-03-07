d9345b57622a2985618d961f14b8d0208a769063
“Vishakha-singal” <vishakhasingal@gmail.com>
2019-03-07 12:04:25 -0700

    Issue #86 Updated model

--- src/edu/asu/heal/reachv3/api/models/SUDSQuestion.java
+++ src/edu/asu/heal/reachv3/api/models/SUDSQuestion.java
@@ -3,11 +3,15 @@ package edu.asu.heal.reachv3.api.models;
 public class SUDSQuestion {
 	
 	String question;
+	String questionType;
+	String response;
 	
 	public SUDSQuestion(){}
 
-	public SUDSQuestion(String question) {
-		this.question=question;
+	public SUDSQuestion(String question, String questionType, String response) {
+		this.question = question;
+		this.questionType = questionType;
+		this.response = response;
 	}
 
 	public String getQuestion() {
@@ -17,5 +21,29 @@ public class SUDSQuestion {
 	public void setQuestion(String question) {
 		this.question = question;
 	}
-	
+
+	public String getQuestionType() {
+		return questionType;
+	}
+
+	public void setQuestionType(String questionType) {
+		this.questionType = questionType;
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+
+	@Override
+	public String toString() {
+		return "SUDSQuestion{" +
+				", question='" + question + '\'' +
+				", questionType='" + questionType + '\'' +
+				", response='" + response + '\'' +
+				'}';
+	}
 }
