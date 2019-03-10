68fdd4105ab50955268904fad662f63668069314
Abhishek <abhishek.uaf30@gmail.com>
2019-03-10 13:49:23 -0700

    Changed schedule for hourly job.

--- src/edu/asu/heal/core/api/resources/HourlyScheduledTask.java
+++ src/edu/asu/heal/core/api/resources/HourlyScheduledTask.java
@@ -18,69 +18,69 @@ public class HourlyScheduledTask extends TimerTask {
 	@Override
 	public void run() {
 		try {
-			List<Patient> patients = reachService.getPatients("5a946ff566684905df608446");
-
-			if(patients != null) {
-				String trialTitle = "Compass"; // Refactor : needs to be done in a better way...
-				SimpleDateFormat timeStampFormat = new SimpleDateFormat("MM.dd.YYYY HH:mm:ss", Locale.US);
-				String date = timeStampFormat.format(new Date());
-				String startMetaData = "", endMetaData="";
-				Logger start,end;
-				Logger[] logs;
-				ArrayList<Logger> al = new ArrayList<Logger>();
-
-				startMetaData = "{ \"Message\" :  \"------------ UX PERSONALIZATION CRON STARTED -----------\" } ";
-				start = new Logger(reachService.getTrialIdByTitle(trialTitle), date, "INFO", "UX_PERSONALIZATION_BEGIN", "JSON",
-						null, null, startMetaData);
-
-				al.add(start);
-				logs = new Logger[al.size()];
-
-				logs = al.toArray(logs);
-				reachService.logPersonalizationMessage(logs);
-				al.clear();
-				for(int i=0;i<patients.size();i++){
-					Integer ppin = patients.get(i).getPin();
-					startMetaData = "{ \"Message\" :  \"UX Personalization started for PIN :"+ppin+"\" } ";
-					start = new Logger(reachService.getTrialIdByTitle(trialTitle), date, "INFO", "UX_PERSONALIZATION_BEGIN", "JSON",
-							null, ppin.toString(), startMetaData);
-
-					al.add(start);
-					logs = new Logger[al.size()];
-
-					logs = al.toArray(logs);
-					reachService.logPersonalizationMessage(logs);
-					boolean rval = reachService.personalizeUserExperience(patients.get(i).getPin());
-
-					if(rval) {
-						endMetaData = "{ \"Message\" :  \"UX Personalization ended for PIN :"+ppin+"\" } ";
-					}else {
-						endMetaData = "{ \"Message\" :  \"UX_PERSONALIZATION - NO SCHEDULE FOR PIN :"+ppin+"\" } ";
-					}
-					end = new Logger(reachService.getTrialIdByTitle(trialTitle), date, "INFO", "UX_PERSONALIZATION_END", "JSON",
-							null, ppin.toString(), endMetaData);
-
-					al.clear();
-					al.add(end);
-					logs = new Logger[al.size()];
-
-					logs = al.toArray(logs);
-					reachService.logPersonalizationMessage(logs);
-				}
-				al.clear();
-				endMetaData = "{ \"Message\" :  \"------------ UX PERSONALIZATION CRON ENDED  -----------\" } ";
-				end = new Logger(reachService.getTrialIdByTitle(trialTitle), date, "INFO", "UX_PERSONALIZATION_END", "JSON",
-						null, null, endMetaData);
-				al.add(end);
-				logs = new Logger[al.size()];
-				logs = al.toArray(logs);
-				reachService.logPersonalizationMessage(logs);
-				now = new Date();
-				System.out.println("Time is :" + now);
-			}else {
-				System.out.println("No Patients in the trial....");
-			}
-
+//			List<Patient> patients = reachService.getPatients("5a946ff566684905df608446");
+//
+//			if(patients != null) {
+//				String trialTitle = "Compass"; // Refactor : needs to be done in a better way...
+//				SimpleDateFormat timeStampFormat = new SimpleDateFormat("MM.dd.YYYY HH:mm:ss", Locale.US);
+//				String date = timeStampFormat.format(new Date());
+//				String startMetaData = "", endMetaData="";
+//				Logger start,end;
+//				Logger[] logs;
+//				ArrayList<Logger> al = new ArrayList<Logger>();
+//
+//				startMetaData = "{ \"Message\" :  \"------------ UX PERSONALIZATION CRON STARTED -----------\" } ";
+//				start = new Logger(reachService.getTrialIdByTitle(trialTitle), date, "INFO", "UX_PERSONALIZATION_BEGIN", "JSON",
+//						null, null, startMetaData);
+//
+//				al.add(start);
+//				logs = new Logger[al.size()];
+//
+//				logs = al.toArray(logs);
+//				reachService.logPersonalizationMessage(logs);
+//				al.clear();
+//				for(int i=0;i<patients.size();i++){
+//					Integer ppin = patients.get(i).getPin();
+//					startMetaData = "{ \"Message\" :  \"UX Personalization started for PIN :"+ppin+"\" } ";
+//					start = new Logger(reachService.getTrialIdByTitle(trialTitle), date, "INFO", "UX_PERSONALIZATION_BEGIN", "JSON",
+//							null, ppin.toString(), startMetaData);
+//
+//					al.add(start);
+//					logs = new Logger[al.size()];
+//
+//					logs = al.toArray(logs);
+//					reachService.logPersonalizationMessage(logs);
+//					boolean rval = reachService.personalizeUserExperience(patients.get(i).getPin());
+//
+//					if(rval) {
+//						endMetaData = "{ \"Message\" :  \"UX Personalization ended for PIN :"+ppin+"\" } ";
+//					}else {
+//						endMetaData = "{ \"Message\" :  \"UX_PERSONALIZATION - NO SCHEDULE FOR PIN :"+ppin+"\" } ";
+//					}
+//					end = new Logger(reachService.getTrialIdByTitle(trialTitle), date, "INFO", "UX_PERSONALIZATION_END", "JSON",
+//							null, ppin.toString(), endMetaData);
+//
+//					al.clear();
+//					al.add(end);
+//					logs = new Logger[al.size()];
+//
+//					logs = al.toArray(logs);
+//					reachService.logPersonalizationMessage(logs);
+//				}
+//				al.clear();
+//				endMetaData = "{ \"Message\" :  \"------------ UX PERSONALIZATION CRON ENDED  -----------\" } ";
+//				end = new Logger(reachService.getTrialIdByTitle(trialTitle), date, "INFO", "UX_PERSONALIZATION_END", "JSON",
+//						null, null, endMetaData);
+//				al.add(end);
+//				logs = new Logger[al.size()];
+//				logs = al.toArray(logs);
+//				reachService.logPersonalizationMessage(logs);
+//				now = new Date();
+//				System.out.println("Time is :" + now);
+//			}else {
+//				System.out.println("No Patients in the trial....");
+//			}
+			System.out.println("Hourly cron timing : " + new Date());
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
